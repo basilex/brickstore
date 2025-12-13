df0fb5d66adb5a7d68938b725cc1dc3735400084
Alexander Vasilenko <alexander.vasilenko@gmail.com>
2025-12-13 18:42:00 +0200

    Rbac role annotations fixed

--- src/main/java/com/platform/brickstore/api/domain/entity/RbacRole.java
+++ src/main/java/com/platform/brickstore/api/domain/entity/RbacRole.java
@@ -23,21 +23,21 @@ import lombok.NoArgsConstructor;
 public class RbacRole {
     @Id
     @Column("id")
-    @NotBlank
+    @NotBlank(message = "RBAC role ID cannot be blank")
     @JsonIgnore
     String id;
 
     @Column("pid")
-    @NotBlank
+    @NotBlank(message = "RBAC PID cannot be blank")
     String pid;
 
     @Column("name")
-    @NotBlank
-    @Size(max = 64)
+    @NotBlank(message = "RBAC role Name cannot be blank")
+    @Size(min = 1, max = 64, message = "RBAC role name must be between 1 and 64 characters")
     String name;
 
     @Column("description")
-    @Size(max = 255)
+    @Size(max = 255, message = "RBAC role description must be less than 255 characters")
     String description;
 
     @Column("created_at")
@@ -45,6 +45,4 @@ public class RbacRole {
 
     @Column("updated_at")
     LocalDateTime updatedAt;
-
-    // ...existing code...
 }
